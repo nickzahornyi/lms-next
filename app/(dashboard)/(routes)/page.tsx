73c2b1073ac3a2c3403f8c679abffdaa1a7331a3
Nick Zahornyi <nick_surveyor@ukr.net>
2023-09-19 22:15:50 +0300

    added auth with clerk

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,5 +1,9 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
   return (
-    <p>This is a protected page</p>
-  )
+    <div>
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
 }
