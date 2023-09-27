dc42d84892a7be7e666c85b57d856f79be2038cc
Nick Zahornyi <nick_surveyor@ukr.net>
2023-09-27 20:36:35 +0300

    dynamic layout

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -1,10 +1,35 @@
 "use client";
 
-import { UserButton } from "@clerk/nextjs";
+import { UserButton, useAuth } from "@clerk/nextjs";
+import { usePathname } from "next/navigation";
+import { LogOut } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export const NavbarRoutes = () => {
+  // const { userId } = useAuth();
+  const pathname = usePathname();
+
+  const isTeacherPage = pathname?.startsWith("/teacher");
+  const isCoursePage = pathname?.includes("/courses");
+
   return (
     <div className="flex gap-x-2 ml-auto">
+      {isTeacherPage || isCoursePage ? (
+        <Link href="/">
+          <Button size="sm" variant="ghost">
+            <LogOut className="h-4 w-4 mr-2" />
+            Exit
+          </Button>
+        </Link>
+      ) : (
+        <Link href="/teacher/courses">
+          <Button size="sm" variant="ghost">
+            Teacher mode
+          </Button>
+        </Link>
+      )}
       <UserButton afterSignOutUrl="/" />
     </div>
   );
