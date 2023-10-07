2ec1879e968ef5fcefdf6d4b5a2eb5906502b74f
Nick Zahornyi <nick_surveyor@ukr.net>
2023-10-07 14:25:46 +0300

    categories query

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -1,5 +1,41 @@
-const SearchPage = () => {
-  return <div>SearchPage</div>;
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+import { db } from "@/lib/db";
+
+import { Categories } from "./_components/categories";
+
+interface SearchPageProps {
+  searchParams: {
+    title: string;
+    categoryId: string;
+  };
+}
+
+const SearchPage = async ({ searchParams }: SearchPageProps) => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const categories = await db.category.findMany({
+    orderBy: {
+      name: "asc",
+    },
+  });
+
+  return (
+    <>
+      <div className="px-6 pt-6 md:hidden md:mb-0 block">
+        {/* TODO: SearchInput */}
+      </div>
+      <div className="p-6 space-y-4">
+        <Categories items={categories} />
+        {/* TODO: CoursesList */}
+      </div>
+    </>
+  );
 };
 
 export default SearchPage;
